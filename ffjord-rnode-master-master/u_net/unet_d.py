9bf198c4d152bdf06cfb40b8a606dd221eae8492
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2021-08-12 14:09:04 +0100

    sa

--- ffjord-rnode-master-master/u_net/unet_d.py
+++ ffjord-rnode-master-master/u_net/unet_d.py
@@ -563,8 +563,9 @@ class G_D(nn.Module):
                 print('target_map: ',target_map.shape)
                 print('x: ', x.shape)
                 print('G_z len: ', len(G_z))
+                print('G_z: ',G_z)
                 #G_z=torch.Tensor(G_z,dtype=torch.float32,device=x)
-                G_z  = torch.from_numpy(np.array(G_z)).to(x)
+                G_z  = torch.from_numpy(np.array(G_z).as_type(np.float32)).to(x)
                 print('G_z: ', G_z.shape)
                 mixed = target_map*x+(1-target_map)*G_z
 
